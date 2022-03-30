2da16c3ac99c68ce5926a678be246e84b5e9445d
koala1707 <m3tpfbzr@gmail.com>
2022-03-30 20:55:01 +1030

    move the input functions into session class

--- Wordle/Guess.h
+++ Wordle/Guess.h
@@ -10,26 +10,27 @@ class Guess
 {
 public:
 	Session *session;
-	void question(string answer,map<int, char> dictionary);
+	void question(string answer,map<int, char> dictionary, string user_ans);
 	string split_input(string input, map<int,char> dic);
 	void show_inputs(string res);
 	int play_game;
 	
 protected:
-	string user_ans;
-	vector<char> user_ans_char;
-	string input;
-	int attempts = 0;
+	//string user_ans;
+	//vector<char> user_ans_char;
+	//string input;
+	//int attempts = 0;
 	string response = "";
 	string res;
 	int skip;
 	int num_guess;
-	map<int, string> user_input = {};
-	void comment(int num);
-	map<int, string> list_comment = {};
+	//map<int, string> user_input = {};
+	vector<string> user_input;
+	//void comment(int num);
+	//map<int, string> list_comment = {};
 	bool check_input(string _input);
 	bool string_check = false;
-	string convert_lower(string user_ans);
+	//string convert_lower(string user_ans);
 
 };
 
