59c800ee66c4bdbf2c4ad9d92e3b5f3a25b40d77
koala1707 <m3tpfbzr@gmail.com>
2022-03-31 19:27:28 +1030

    add infinite loop

--- Wordle/Game.h
+++ Wordle/Game.h
@@ -9,22 +9,22 @@ class Game
 public:
 	void menu();
 	int menu_number = 0;
+	bool no_exit = true;
 	int* chose_ptr = &chose_play;
 	
 protected:
+	map<int, string> map_menu;
 	int chose_play = 0;
 	void stats();
 	void help();
 	vector<int> show_guess_distribution(int attempt, bool won);
-	//map<int, int> list_distribution;
 	vector<int> result_distribution;
 	bool check_win =false;
-	int played = 0;
+	//int played = 0;
 	int num_won=0;
 	int streak=0;
 	int max_streak=0;
 	double percent_won = 0.0;
-	map<int, int> won_attempts ={};
 	vector<int> list_distribution = {0,0,0,0,0,0};
 };
 
