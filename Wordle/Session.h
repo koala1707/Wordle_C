0bbf258ece3fdf94cf470753e4cf8401f717ce47
koala1707 <m3tpfbzr@gmail.com>
2022-03-31 20:37:49 +1030

    change c++11 style

--- Wordle/Session.h
+++ Wordle/Session.h
@@ -14,16 +14,16 @@ public:
 	Guess guess;
 
 	bool won_res;
-	int attempts = -1;
-	int* attempts_ptr = &attempts;
+	int attempts{ -1 };
+	int* attempts_ptr{ &attempts };
 
 protected:
 	string user_ans;
 	string input;
 	string expectation;
 	vector<char> user_ans_char;
-	map<int, string> list_comment = {};
-	map<int, char> ans_map = {};
+	map<int, string> list_comment{};
+	map<int, char> ans_map{};
 
 	map<int, char> get_word(int num);
 	string convert_lower(string user_ans);
