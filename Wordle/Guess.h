963ce50f943386d3c67e24d7d535a80b2792ea78
koala1707 <m3tpfbzr@gmail.com>
2022-03-31 20:13:38 +1030

    refine code

--- Wordle/Guess.h
+++ Wordle/Guess.h
@@ -12,13 +12,17 @@ public:
 	void question(string answer,map<int, char> dictionary, string user_ans);
 	
 protected:
+	bool input_type;
+	string response = "";
+	bool duplicate;
+	int skip;
+	vector<string> user_input;
+	string split_res;
+	bool string_check = false;
+
 	bool check_input(string _input);
 	string split_input(string input, map<int, char> dic);
 	void show_inputs(string res);
-	vector<string> user_input;
-	string res;
-	string response = "";
-	int skip;
-	bool string_check = false;
+	
 };
 
